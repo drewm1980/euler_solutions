9f11c2c5c89c923c5d523473814f2ebae82101d1
Andrew Wagner <andrew.wagner@esat.kuleuven.be>
2014-06-04 11:32:29 +0200

    Solved 001

--- 001.hs
+++ 001.hs
@@ -1,4 +1,14 @@
 
 main :: IO () 
 main = do
-    putStrLn "hellooo"
+    let result = solve 999
+    putStrLn $ "The solution is " ++ show result
+
+-- Find the sum of all the multiples of 3 or 5 below 1000.
+
+filt :: Int -> Bool
+filt x = (mod x 3 == 0) || (mod x 5 == 0)
+
+solve :: Int -> Int
+solve x = sum $ filter filt [1..x]
+
